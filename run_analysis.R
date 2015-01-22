476b1dae8716de152b29553190c6663dc150ec70
WYH OsElite <wyh.oselit@gmail.com>
2015-01-22 14:24:54 +0800

    Download Data files and delete it.

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,12 @@
 # You should create one R script called run_analysis.R that does the following. 
 # * Merges the training and the test sets to create one data set.
+
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",destfile=temp,method="curl")
+
+unlink(temp)
+
+
 # * Extracts only the measurements on the mean and standard deviation for each measurement. 
 # * Uses descriptive activity names to name the activities in the data set
 # Appropriately labels the data set with descriptive variable names. 
