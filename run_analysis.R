1c3e19f6020fcc637856ffbff0b013d500a1f569
WYH OsElite <wyh.oselit@gmail.com>
2015-01-22 14:52:08 +0800

    Combine labeled Data

--- run_analysis.R
+++ run_analysis.R
@@ -32,6 +32,10 @@ names(XData) <- features[indexFocus,2]
 
 # * Uses descriptive activity names to name the activities in the data set
 YData[,1] = activityLabels[YData[,1],2]
+names(YData) <- "Activity"
 
+names(Subject) <- "Subject"
 # Appropriately labels the data set with descriptive variable names. 
+labeledData <- cbind(Subject,XData,YData)
+
 # * From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
